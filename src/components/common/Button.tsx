9ecd8e94fa68fcff667b7f2ff15503ea641ca32e
ArtycoBW <artyco.bw@gmail.com>
2024-10-05 15:01:32 +0300

    добавление основных переменных стилей и компонентов Button и Input

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,7 +1,21 @@
+'use client'
 import React from 'react'
+import styles from '@styles/Button.module.scss'
 
-const Button = () => {
-  return <div>Button</div>
+interface ButtonProps {
+  width?: string
+  height?: string
+  style?: React.CSSProperties
+  onClick?: () => void
+  children: React.ReactNode
+}
+
+const Button: React.FC<ButtonProps> = ({ width = '273px', height = '48px', style, onClick, children }) => {
+  return (
+    <button className={styles.button} style={{ width, height, ...style }} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+'use client'
 
-const Input = () => {
-  return <div>Input</div>
+import React from 'react'
+import styles from '@styles/Input.module.scss'
+
+interface InputProps {
+  width?: string
+  height?: string
+  style?: React.CSSProperties
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  placeholder?: string
+}
+
+const Input: React.FC<InputProps> = ({ width, height, style, onChange, value, placeholder='Введите данные' }) => {
+  return (
+    <input
+      className={styles.input}
+      style={{ width, height, ...style }}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+    />
+  )
 }
 
 export default Input
